d49375102dbff6f99f9cd655cc207b5172ece89d
Amrit-Giri <85377404+Amrit-Giri@users.noreply.github.com>
2022-04-05 22:31:53 +0545

    Update login.sh

--- login.sh
+++ login.sh
@@ -51,12 +51,13 @@ echo -e "\033[1m\033[33m
 ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║
 ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ 
 "
-echo -e  "     \e[1m\e[32m▂▃▄▅▆▇▓▒░ \033[1mCoded By \e[33mAmrit \e[1m\e[32m░▒▓▇▆▅▄▃▂"
+echo -e  "     \e[1m\e[32m▂▃▄▅▆▇▓▒░ \033[1mCoded By \e[33m4mr!t-G!r! \e[1m\e[32m░▒▓▇▆▅▄▃▂"
 cd /sdcard
 python network.py
 echo -e "   \033[1m\033[33m]\033[31m─────────────────────────────────────\033[33m["
 echo 
 PS1='\033[1m\[\e[32m\]\033[1m┌─[\[\e[37m\]\T\[\e[32m\]\033[1m]─────\033[1m\e[1;98m\[[\033[1m\033[37m$names\033[32m]\033[1m\e[0;32m\033[1m───[\033[38;5;209m\#\033[32m]\n|\n\033[1m\e[0;32m\033[1m└─[\[\e[32m\]\e[1;33m\W\[\e[1m\033[32m]\033[1m────►\e[1;93m\033[1m '
+termux-tts-speak Login Succesfully.
 termux-tts-speak Welcome $username.
 << comment
 shopt -s autocd
