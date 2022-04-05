279cd54afb57de545047b29510fc38277491400e
Amrit-Giri <85377404+Amrit-Giri@users.noreply.github.com>
2022-04-05 22:14:48 +0545

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,36 @@
+
+#!/bin/bash 
+clear
+echo
+pkg install pv -y >/dev/null 2>&1
+echo -e "\033[32m\033[1m]────────────────────────────────────────────["
+echo -e "\033[33m\033[1m   Installing Packages and All Dependencies" | pv -qL 10
+apt update && apt upgrade -y 
+pkg install python -y 
+pkg install cmatrix -y 
+pkg install pv -y 
+apt install bash
+apt install figlet -y  
+apt install ruby -y 
+apt install mpv -y 
+pip install lolcat 
+pip install random 
+pip install requests 
+pkg install python2 -y 
+pkg install termux-api -y 
+echo -e "\033[31m\033[1m        INSTALLED SUCCESSFULLY \033[32m[\033[36m✓\033[32m]" | pv -qL 12
+echo -e "\033[33m\033[1m]────────────────────────────────────────────["
+termux-setup-storage
+cd $HOME 
+cd Termux-Login 
+cp login.sh $PREFIX/etc
+cd $HOME
+cd Termux-Login 
+chmod +x login.sh delete.sh setup.sh sound_effect.py banner.sh 
+rm 1
+mv network.py /sdcard
+
+cd $HOME 
+cd Termux-Login 
+
+bash login.sh
