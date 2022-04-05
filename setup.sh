17b9e3de08d7925cc8bd965267f37c0531939b76
Amrit-Giri <85377404+Amrit-Giri@users.noreply.github.com>
2022-04-05 23:13:30 +0545

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,10 +22,10 @@ echo -e "\033[31m\033[1m        INSTALLED SUCCESSFULLY \033[32m[\033[36m✓\033[
 echo -e "\033[33m\033[1m]────────────────────────────────────────────["
 termux-setup-storage
 cd $HOME 
-cd Termux-Login 
+cd termux-login
 cp login.sh $PREFIX/etc
 cd $HOME
-cd Termux-Login 
+cd termux-login 
 chmod +x login.sh delete.sh setup.sh sound_effect.py banner.sh 
 mkdir Song 
 mv Access-Granted.mp3 Jarvis2.mp3 JARVIS.mp3 sound_effect.py Song
@@ -33,6 +33,6 @@ rm 1
 mv network.py /sdcard
 
 cd $HOME 
-cd Termux-Login 
+cd termux-login 
 
 bash login.sh
